24bb671c58c0019786c48ce73304f57c6214235b
hanna <82633682+hanna-tes@users.noreply.github.com>
2025-08-27 12:54:20 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+# IMPORTANT: Replace the placeholder with your actual NewsAPI key.
+# This file should not be committed to a public repository.
+NEWSAPI_KEY = "5025ab0d66d940929c2642e4bfc786bb"
